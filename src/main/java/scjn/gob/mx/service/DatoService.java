9d30dd678bdb758f6995372d3e5b502f8e01048b
jelinarest <56939378+jelinarest@users.noreply.github.com>
2019-10-24 11:32:02 -0500

    Update DatoService
    Adición de línea de prueba

--- src/main/java/scjn/gob/mx/service/DatoService.java
+++ src/main/java/scjn/gob/mx/service/DatoService.java
@@ -62,6 +62,7 @@ public class DatoService {
     @Transactional(readOnly = true)
     public Page<DatoDTO> findAll(Pageable pageable) {
         log.debug("Request to get all Datoes");
+        log.debug("Test");
         return datoRepository.findAll(pageable)
             .map(datoMapper::toDto);
     }
